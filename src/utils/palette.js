6fc3a58240ce8e2be1971815ac5ab93cd4141279
Okan <42724589+dovenokan@users.noreply.github.com>
2022-04-08 13:42:16 +0300

    reddit 2022 day 1 colors

--- src/utils/palette.js
+++ src/utils/palette.js
@@ -1,14 +1,21 @@
 
 const colors = {
-    red:"red",
-    green:"green",
-    blue:"blue",
-    yellow:"yellow",
-    orange:"orange",
-    purple:"purple",
-    pink:"pink",
-    black:"black",
-    white:"white",
+    c1:"#ff4500",
+    c2:"#ffa800",
+    c3:"#ffd635",
+    c4:"#00cc78",
+    c5:"#7eed56",
+    c6:"#2450a4",
+    c7:"#3690ea",
+    c8:"#51e9f4",
+    c9:"#811e9f",
+    c10:"#b44ac0",
+    c11:"#ff99aa",
+    c12:"#9c6926",
+    c13:"#000000",
+    c14:"#898d90",
+    c15:"#d4d7d9",
+    c16:"#ffffff",
 }
 
 export {
